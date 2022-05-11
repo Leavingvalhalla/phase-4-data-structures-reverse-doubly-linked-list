d55e11369aa2538c028fbeb6f10f6244f8f13255
Zach Lefler <leaving.valhalla@gmail.com>
2022-05-11 15:13:37 -0700

    commit tree 2efa04004febbb8f3b30cc0a267a8af75921d5f6

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -9,6 +9,23 @@ class LinkedList
   end
 
   def reverse!
-    # your code here
+# skip head, start iwht head.next
+# reverse next and previous on node, then move to next one
+#get to old_tail, rename both and get rid of their old next/prev, and add proper prev/next
+    return if @head == @tail
+    curr = @head.next_node
+    @head.prev_node = @head.next_node
+    @head.next_node = nil
+    until curr == @tail
+     placeholder = curr.next_node 
+     curr.next_node = curr.prev_node
+     curr.prev_node = placeholder
+     curr = curr.prev_node
+    end
+    @tail.next_node = @tail.prev_node
+    @tail.prev_node = nil
+    placeholder = @head
+    @head = @tail
+    @tail = placeholder
   end
 end
